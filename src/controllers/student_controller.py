a083302c5278d639410b321b9aa27def7edd0f46
rambabupathakamuri <rpathakamuri@rivier.edu>
2024-12-07 04:20:24 +0000

    updating purpose

--- src/controllers/student_controller.py
+++
@@ -1,8 +0,0 @@
-# student_controller.py
-from flask import Blueprint
-
-student_bp = Blueprint('student_bp', __name__)
-
-@student_bp.route('/student/dashboard')
-def student_dashboard():
-    return "Student Dashboard"
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from src.controllers.student_controller import student_bp
-
-app = Flask(__name__)
-app.register_blueprint(student_bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
